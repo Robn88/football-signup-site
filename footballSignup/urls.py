e691f01fdacbe670d1f2e43399da7f8f1c83c054
Robn88 <robert.d.nicholas@gmail.com>
2022-04-12 14:41:06 +0000

    Install allauth and update settings.py

--- footballSignup/urls.py
+++ footballSignup/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('signup.urls'), name='signup_urls')
+    path('', include('signup.urls'), name='signup_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
