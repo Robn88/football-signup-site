0b078c3c19b3ea2a91cd2490fbfe13e10ff6459a
Robn88 <robert.d.nicholas@gmail.com>
2022-04-12 10:08:40 +0000

    Render EventList view and connect URLs

--- signup/admin.py
+++ signup/admin.py
@@ -4,7 +4,9 @@ from .models import Event
 
 class Filter(admin.ModelAdmin):
 
+    list_display = ('title', 'event_date_and_time', 'venue')
     list_filter = ('venue', 'event_date_and_time', 'title')
+    search_fields = ('venue', 'event_date_and_time', 'created_by')
 
 
 admin.site.register(Event, Filter)
--- signup/views.py
+++ signup/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Event
 
-# Create your views here.
+
+class EventList(generic.ListView):
+    model = Event
+    queryset = Event.objects.order_by('-event_date_and_time')
+    template_name = 'index.html'
+    paginate_by = 6
