ec811cd72203c3baccc2589ab57ed85f03418c4b
Robn88 <robert.d.nicholas@gmail.com>
2022-04-14 15:00:16 +0000

    Configure path for event registration

--- signup/urls.py
+++ signup/urls.py
@@ -4,8 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.EventList.as_view(), name='Home'),
-    path('event>/<int:id>', views.EventDetail.as_view(), name='event_detail')
+    path('event/<int:id>', views.EventDetail.as_view(), name='event_detail'),
+    path("event/registration/<int:id>", views.event_registration, name='event_registration'),
 ]
-
-
-# '<title:title>/'
