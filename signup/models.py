04c84fc77b8749d8d940e83e052caef2bed37ac2
Robn88 <robert.d.nicholas@gmail.com>
2022-04-29 13:09:51 +0000

    Make code pep8 compliant

--- signup/models.py
+++ signup/models.py
@@ -4,7 +4,8 @@ from django.contrib.auth.models import User
 
 class Event(models.Model):
     title = models.CharField(max_length=100)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="event_posts")
+    created_by = models.\
+        ForeignKey(User, on_delete=models.CASCADE, related_name="event_posts")
     event_date_and_time = models.DateTimeField()
     venue = models.CharField(max_length=100)
     max_participants = models.IntegerField()
@@ -16,16 +17,16 @@ class Event(models.Model):
 
     def __str__(self):
         return self.title
-    
+
 
 class Registration(models.Model):
 
-    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="event_registration")
+    event = models.\
+        ForeignKey(Event, on_delete=models.CASCADE, related_name="event_registration")
     name = models.ForeignKey(User, on_delete=models.CASCADE)
     ball = models.BooleanField(default=False)
     bibs = models.BooleanField(default=False)
     guest = models.BooleanField(default=False)
 
-    
     def __str__(self):
         return str(self.name)
