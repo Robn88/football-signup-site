70234c1ad33a3005ec87a1f704e4a433eb840f61
Robn88 <robert.d.nicholas@gmail.com>
2022-05-02 17:16:13 +0000

    Add comments to views.py

--- signup/views.py
+++ signup/views.py
@@ -8,6 +8,13 @@ from .models import Event, Registration
 from .forms import RegistrationForm
 
 
+'''
+This class based view contains two querysets to sort the events
+into future and past, and then display them in alphabetical order
+on index.html.
+'''
+
+
 class EventList(generic.ListView):
     context_object_name = "data"
     template_name = 'index.html'
@@ -23,6 +30,18 @@ class EventList(generic.ListView):
             .order_by('event_date_and_time'),
         }
         return context
+
+
+'''
+This class based view displays all the information about
+a particular event. The event is identified from its
+unqiue pk. Then, the people who have signed up are
+obtained, and for each of these registrations, the
+total particpants is increased by 1, or by 2 if they
+are bringing a guest. Finally, the remaining spaces
+are calculated by subtracting the current number of
+participants from the maximum number.
+'''
 
 
 class EventDetail(View):
@@ -45,6 +64,16 @@ class EventDetail(View):
         return render(request, template, context)
 
 
+'''
+This function based view, which has a login required
+decorator, enables users to register for an event. The
+event is again identified by its pk, and the registration
+form is loaded, and if filled in, is saved. The user is
+then redirected to the page of the event that he/she had
+signed up for.
+'''
+
+
 @login_required
 def event_registration(request, id):
     event = get_object_or_404(Event, pk=id)
@@ -62,6 +91,20 @@ def event_registration(request, id):
         "form": form,
     }
     return render(request, template, context)
+
+
+'''
+This function based view, which also has a login
+required decorator, enables users to unregister
+for an event. It fetches the event, but also the
+unique id associated with an individual
+registration. It then checks to make sure that
+the name corresponding to the registration matches
+that of the user making the request, and deletes
+it if is. The user is then redirected to the
+event detail page of the event that they have just
+unregistered from.
+'''
 
 
 @login_required
